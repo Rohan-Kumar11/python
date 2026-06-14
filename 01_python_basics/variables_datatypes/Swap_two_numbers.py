4b263a2913617312933e8cd8a6fd53814e404f2c
Rohan Kumar <rohankumar971161@gmail.com>
2026-06-15 04:00:49 +0530

    Swap two numbers

---
+++ 01_python_basics/variables_datatypes/Swap_two_numbers.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+print(f"Before Swapping: a={a},b={b}")
+temp=a;
+a=b;
+b=temp;
+print(f"After Swapping: a={a},b={b}")
