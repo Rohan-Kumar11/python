83eaa11df6e43be5173898b2ea70e4afbd7cd22f
Rohan Kumar <rohan971161@gmail.com>
2026-06-30 00:16:52 +0530

    Loop- prime checker

---
+++ 01_python_basics/Loops/Prime_checker.py
@@ -0,0 +1,7 @@
+n = int(input("Enter the number in prime checker : "))
+for i in range(2,n):
+    if n%i == 0 :
+        print(f"The number {n} is not prime .")
+        break
+else:
+    print(f"The number {n} is prime.")
